df43654fd414a16e8803aee6d4ac7a542646663e
Michael Symmes <msymmes@gmail.com>
2019-05-15 19:36:57 -0400

    Code cleanup.

--- src/draggable.js
+++ src/draggable.js
@@ -2,35 +2,29 @@
 // modified the pannable code from https://svelte.dev/tutorial/actions
 // TODO: give it bounding boxes (window size)
 // TODO: make it only droppable in certain locations
+/**
+ * 
+ * @param {*} node  - The node to make draggable
+ * @param {*} options  - Options to use for draggings
+ * 
+ * @param options
+ * {
+ *  boundingBox: Bool: Whether or not to bind the draggable element to a parent container.
+ *  container: HTMLElement: The element to keep node inside of
+ * }
+ */
+
 export function draggable(node, options = {}) {
     let x;
     let y;
 
     const nodePosition = { x: 0, y: 0 };
-    function handleMousedown(event) {
-        x = event.clientX;
-        y = event.clientY;
-
-        node.dispatchEvent(new CustomEvent('dragstart', {
-            detail: { x, y }
-        }));
-
-        window.addEventListener('mousemove', handleMousemove);
-        window.addEventListener('mouseup', handleMouseup);
-    }
-
-    function handleMousemove(event) {
+    const setNodePosition = (dx, dy) => {
         const { width, height } = node.getBoundingClientRect();
-        const dx = event.clientX - x;
-        const dy = event.clientY - y;
-        x = event.clientX;
-        y = event.clientY;
-
         let nextX = nodePosition.x + dx;
         let nextY = nodePosition.y + dy;
         const container = options.container || window;
         let containerWidth, containerHeight;
-        console.log(container);
         if (container === window) {
             containerWidth = window.innerWidth;
             containerHeight = window.innerHeight;
@@ -49,6 +43,27 @@ export function draggable(node, options = {}) {
             nodePosition.x = nextX;
             nodePosition.y = nextY;
         }
+    };
+
+    function handleMousedown(event) {
+        x = event.clientX;
+        y = event.clientY;
+
+        node.dispatchEvent(new CustomEvent('dragstart', {
+            detail: { x, y }
+        }));
+
+        window.addEventListener('mousemove', handleMousemove);
+        window.addEventListener('mouseup', handleMouseup);
+    }
+
+    function handleMousemove(event) {
+        const dx = event.clientX - x;
+        const dy = event.clientY - y;
+        x = event.clientX;
+        y = event.clientY;
+
+        setNodePosition(dx, dy);
 
         node.dispatchEvent(new CustomEvent('dragmove', {
             detail: { x, y, dx, dy }
